28e039d62ad6c606dc11a84155985825d74540f0
wenmingyuan <wenmingyuan12@126.com>
2021-08-28 23:42:03 +0800

    解决 876_链表的中间结点

---
+++ 解题/876_链表的中间结点.js
@@ -0,0 +1,19 @@
+function ListNode(val, next) {
+  this.val = (val===undefined ? 0 : val)
+  this.next = (next===undefined ? null : next)
+}
+
+
+/* 自己的思路 快慢指针  通过  和答案一样
+  初始时，让指针 pre 和 cur 同时指向链表第一个结点。
+  让它们同时向后移动，cur 的移动速度是 pre 的 2 倍。
+  当 cur 指向 null（有 2 个中间结点的情况） 或 cur.next 指向 null 时（有 1 个中间结点的情况），返回 pre
+*/
+var middleNode = function(head) {
+  let pre = head, cur = head;
+  while (cur && cur.next) {
+    pre = pre.next;
+    cur = cur.next.next;
+  }
+  return pre;
+}
