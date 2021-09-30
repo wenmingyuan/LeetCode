7f249782ff27ab81513e34dadaeaaedb39b92c33
wenmingyuan <wenmingyuan12@126.com>
2021-09-30 21:02:48 +0800

    补充 876_链表的中间结点

--- 解题/链表/876_链表的中间结点.js
+++ 解题/链表/876_链表的中间结点.js
@@ -8,6 +8,12 @@ function ListNode(val, next) {
   初始时，让指针 pre 和 cur 同时指向链表第一个结点。
   让它们同时向后移动，cur 的移动速度是 pre 的 2 倍。
   当 cur 指向 null（有 2 个中间结点的情况） 或 cur.next 指向 null 时（有 1 个中间结点的情况），返回 pre
+
+  这种做法是让快慢指针初始都指向第一个结点，如果有 2 个中间结点，返回的是中间偏后的结点
+  如 [1,2,3,4,5,6]，返回的是 4
+
+  或者可以使用 dummy 结点，如果有 2 个中间结点，返回的是中间偏前的结点
+  如 [1,2,3,4,5,6]，返回的是 3
 */
 var middleNode = function(head) {
   let pre = head, cur = head;
